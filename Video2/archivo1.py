89f394a003cb7539eb623ea7a02c9d5922d86884
TuringCode9 <danielguillermoprogramming@gmail.com>
2022-05-16 14:59:28 -0500

    Primera Version

---
+++ Video2/archivo1.py
@@ -0,0 +1,3 @@
+print("Estamos iniciando en Python")
+for i in range(10):
+    print(i)
---
+++ Video6/ProyectoWebEmpresaB/js/code.js
@@ -0,0 +1,19 @@
+
+function saludo(){
+
+    alert ("Hola alumnos")
+
+}
+
+function estamos_aprendiendo(){
+
+    alert ("Estamos aprendiendo a usar las branch de Git")
+
+}
+
+function Fibonacci(){
+    alert ("Función Fibinacci ")
+
+}
+
+
